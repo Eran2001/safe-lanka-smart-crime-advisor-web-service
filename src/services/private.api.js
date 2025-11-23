a9b22e5102637121d91c16043337b7948452f99f
M-E-Hasareli <eran.hasarali.2001@gmail.com>
2025-11-23 12:23:55 +0530

    add pages and some compos

---
+++ src/services/private.api.js
@@ -0,0 +1,31 @@
+import instance from "../lib/axios";
+
+/*
+  User Onboarding Component APIs - APIs Used in User Onboarding Component
+*/
+const onboarding_userRegister = async (params) => {
+  return await instance.clientOnboarding.post("/auth/register", params, {
+    headers: instance.defaultHeaders(),
+  });
+};
+
+const onboarding_userLogin = async (params) => {
+  return await instance.clientOnboarding.post("/auth/login", params, {
+    headers: instance.defaultHeaders(),
+  });
+};
+/*
+  End of User Onboarding Component APIs - End of APIs Used in User Onboarding Component
+*/
+
+const privateAPI = {
+  /*
+    User Onboarding Component APIs - APIs Used in User Onboarding Component
+  */
+  onboarding_userRegister,
+  /*
+    End of User Onboarding Component APIs - APIs Used in User Onboarding Component
+  */
+};
+
+export default privateAPI;
